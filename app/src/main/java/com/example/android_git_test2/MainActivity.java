eaa6c89c9404855b77f18080f82fe8b8b8f63d0b
smhrd <jhy2639@naver.com>
2021-05-13 12:39:03 +0900

    TEST1

--- app/src/main/java/com/example/android_git_test2/MainActivity.java
+++ app/src/main/java/com/example/android_git_test2/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.android_git_test2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.button);
+
     }
 }
