527313df2c9b79c5707d4ca1ca51a4e488548f8f
jhy2639 <55908382+jhy2639@users.noreply.github.com>
2021-05-13 13:05:36 +0900

    hi hi

--- app/src/main/java/com/example/android_git_test2/MainActivity.java
+++ app/src/main/java/com/example/android_git_test2/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                int a = 1;
+
             }
         });
 
